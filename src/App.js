1036bb8d460f4060e35f2c81352a55f4e1183819
Tushar <vaniyatushar99@gmail.com>
2023-10-23 19:41:51 +0530

    Video 5 completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
 function App() {
   return (
-    <div className="App">
+    // Code written bellow is called JSX
+    // we can only return only 1 component in function 
+    // syntax of JSX is same as HTML just we change class-->className
+    //for ---> htmlFor
+    //tab ---> tabIndex
+    // if we want to return more than 1 component then we have to use JSX fragment
+    <>
+    {/* this is component 1 */}
+    {/* <h1>Tushar</h1> */}
+
+    {/* this is component 2 */}
+    {/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,10 +25,37 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Tushar
         </a>
       </header>
+    </div> */}
+
+
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Textutils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
+    
+    
+    </>
   );
 }
 
